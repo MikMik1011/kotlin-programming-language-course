a52006554c2e9722ca1e5bba3eafa0127099b12c
Miša Stefanović <mikmikyt1011@gmail.com>
2024-05-20 17:41:10 +0200

    add solutions for exercise 9

--- src/main/kotlin/exercise9/EmployeesRunner.kt
+++ src/main/kotlin/exercise9/EmployeesRunner.kt
@@ -3,11 +3,21 @@ package exercise9
 import common.FileReader
 
 fun parseEmployees(employeesCSVLines: List<String>): List<Employee> {
-    TODO("Implement parsing of employees")
+    return employeesCSVLines.
+            drop(1).
+            map {
+                val components = it.split(",")
+                val id = EmployeeID(components[0].trim())
+                val name = components[1].trim()
+                val department = Department(components[2].trim())
+                val salary = components[3].trim().toInt()
+                val skills = components[4].split("|")
+                Employee(id, name, department, salary, skills)
+            }
 }
 
 fun newEmployeeApi(employees: List<Employee>): EmployeeApi {
-    TODO("Instantiate EmployeeApi")
+    return Employees(employees)
 }
 
 fun main() {
